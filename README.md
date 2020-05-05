b2a483181a8b2a22baf78f90d2ccca9c94a640b8
davinci <sotirisnan@gmail.com>
2020-05-04 19:13:47 -0700

    v1

---
+++ README.md
@@ -0,0 +1,7 @@
+## <Name of the article>
+
+## How to run the code:
+
+## Disclaimer
+All writers' opinions are their own and do not constitute financial advice in any way whatsoever. Nothing published by the writer constitutes an investment recommendation, nor should any data or Content published by the writer be relied upon for any investment activities.
+Past performance is not indicative of future results, prices/invested sum is subject to market risks which may result in appreciation or depreciation. The ownership of any investment decision(s) shall exclusively vest with the Investor after analysing all possible risk factors and by exercise of his/her/its independent discretion. I strongly recommends that you perform your own independent research and/or speak with a qualified investment professional before making any financial decisions.
--- analyze.py
+++ analyze.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+from dataclasses import dataclass
+import os.path
 import argparse
 import math
 from collections import defaultdict
@@ -8,13 +10,11 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set(rc={'figure.figsize': (11, 4)})
 
-dataset_path = "./data/"
-save_dir = './out/'
-
 hedging_percentage = 100
 initial_investment = 5000
+reInvesting = True
 
-subsetSize = 100000
+subsetSize = None
 
 
 def growth(final, start):
@@ -23,6 +23,109 @@ def growth(final, start):
 
 def cagr(end, start, days):
     return ((end / start)**(365 / days) - 1)*100
+
+
+def simulate(ema_span_low, ema_span_high, ema_span_long_term, subset, fund_days, verbose=False):
+
+    price_at_start = subset.iloc[0].values[0]
+    price_at_end = subset.iloc[-1].values[0]
+    shares_owned = math.ceil(initial_investment/price_at_start)
+
+    # Calculation of indices
+    narrow_ema = subset.ewm(span=ema_span_low, adjust=False).mean()
+
+    wide_ema = subset.ewm(span=ema_span_high, adjust=False).mean()
+
+    long_term_ema = subset.ewm(
+        span=ema_span_long_term, adjust=False).mean()
+
+    # Hedging Strategy Analysis
+    if verbose:
+        print("=================== HEDGING ANALYSIS ============================")
+    """
+    You are hedged when:
+        1. narrow EMA < wide EMA
+        2. price < long_term_ema
+    """
+    hedged = (subset < long_term_ema) & (narrow_ema <= wide_ema)
+    hedged_shifted_r = hedged.shift(1)
+    hedged_shifted_l = hedged.shift(-1)
+
+    hedge_start = hedged_shifted_l & (~hedged)
+    hedge_start = hedge_start.loc[hedge_start['Close/Last'] == True]
+    hedge_end = hedged_shifted_r & (~hedged)
+    hedge_end = hedge_end.loc[hedge_end['Close/Last'] == True]
+
+    # TODO: There might be an awkard case where the do the backtesting while
+    # we already hedged. In that case this might break since the dataframes
+    # do not have the same shape.
+    if verbose:
+        print("Fund duration: {} days (aprx {:.2f} years)".format(
+            fund_days, fund_days / 365))
+        print("Initial capital: {}".format(initial_investment))
+
+        print("Hyper-parameters: (Narrow window, Wide window, Long Term window) ({},{},{}))".format(
+            ema_span_low, ema_span_high, ema_span_long_term))
+        if reInvesting:
+            print("Profits/losses made from hedging will be re-invested in the fund")
+        else:
+            print("Profits/losses made will be kept as cash")
+
+    cash = 0
+    current_shares = shares_owned
+    annual_profits = defaultdict(int)
+    for i in range(0, min(hedge_start.shape[0], hedge_end.shape[0])):
+        start = hedge_start.iloc[i].name
+        end = hedge_end.iloc[i].name
+        end_year = end.strftime('%Y')
+        hedgeProfits = short_shell(start, end, subset,
+                                   current_shares, hedging_percentage)
+        annual_profits[end_year] += hedgeProfits
+        if reInvesting:
+            price_at_hedge_end = subset.loc[end].values[0]
+            # If profits from hedging are positive move again to the position
+            if hedgeProfits > 0:
+                cash += hedgeProfits
+                shares = math.floor(cash / price_at_hedge_end)
+                current_shares += shares
+                cash -= shares * price_at_hedge_end
+            else:
+                shares = math.ceil(abs(hedgeProfits) / price_at_hedge_end)
+                current_shares -= shares
+                residual = shares * price_at_hedge_end - hedgeProfits
+                cash += residual
+                if current_shares < 0:
+                    raise ValueError(
+                        "Simulation lead to bankcrupcy at hedge {} - {}".format(start, end))
+        else:
+            cash += hedgeProfits
+
+    hold_value = price_at_end * shares_owned
+    hold_cagr = cagr(hold_value, initial_investment, fund_days)
+    hedged_portofolio_value = price_at_end * current_shares
+    hedged_terminal_value = hedged_portofolio_value + cash
+    hedged_cagr = cagr(hedged_terminal_value, initial_investment, fund_days)
+
+    if verbose:
+        print("Results Overview:")
+        print("-> Terminal value with Long & Hold {:.2f} and a CAGR of {:.2f}%".format(
+            hold_value, hold_cagr))
+        print("-> Terminal value with Hedges {:.2f} (Portofolio: {:.2f} Cash: {:.2f}) and a CAGR of {:.2f}%".format(
+            hedged_terminal_value, hedged_portofolio_value, cash, hedged_cagr))
+
+        if hedged_cagr > hold_cagr:
+            print(
+                "-> Hedging outperforms Long & Hold by {:.2f}x".format(hedged_terminal_value/hold_value))
+        else:
+            print(
+                "-> Long & Hold outperforms hedging by {:.2f}x".format(hold_value/hedged_terminal_value))
+        print("Hedging Cost Breakdown per Year")
+        for key in annual_profits:
+            print("-> Year: {} - Profit/Loss: {:.2f} ".format(key,
+                                                              annual_profits[key]))
+        print("=================================================================")
+
+    return hedged_cagr, hedged_portofolio_value, cash, hold_value, hold_cagr, annual_profits,  narrow_ema, wide_ema, long_term_ema, hedged
 
 
 def short_shell(dateStart, dateEnd, priceSet, shares_owned, hedging_percentage):
@@ -45,158 +148,113 @@ def short_shell(dateStart, dateEnd, priceSet, shares_owned, hedging_percentage):
 
     profit = cash_at_start - cash_end_required
 
-    # print("Profits/Losses in hedging period {} - {} are {:.2f}".format(dateStart.strftime(
-    #     '%d %B %Y'), dateEnd.strftime(
-    #     '%d %B %Y'), profit))
-
     return profit
-
-
-def simulate()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Command Line Interface')
-    parser.add_argument('-d', '--dataset', type=str, nargs='?')
+    parser.add_argument('-d', '--dataset', type=str,
+                        nargs='?', help='Path to the dataset')
     parser.add_argument('-v', '--visualize', action='store_true', default=False,
                         help='Visualize data')
+    parser.add_argument('-p', '--optimize', action='store_true', default=False,
+                        help='Optimize the hyper-parameters of the simulation')
     args = parser.parse_args()
 
-    dataset = args.dataset.lower()
+    dataset_path = "./data/"
 
     col_list = ["Date", "Close/Last"]
+    if not args.dataset or not os.path.exists(args.dataset.lower()):
+        print("ERROR: The path {} does not seem to exist".format(args.dataset))
+        print("You can download the data from https://www.nasdaq.com/market-activity/funds-and-etfs/iwo/historical")
+        exit(1)
 
-    historicalData = pd.read_csv(
-        dataset_path + dataset, usecols=col_list, header=0, parse_dates=[0], index_col=0, skipinitialspace=True)
+    dataset = args.dataset.lower()
+    try:
+        historicalData = pd.read_csv(
+            dataset, usecols=col_list, header=0, parse_dates=[0], index_col=0, skipinitialspace=True)
+    except:
+        print("ERROR: Failed to load {} as CSV".format(dataset))
+        print("Make sure it follows the format (Date, Close/Last, Volume, Open, High, Low)")
+        print("You can download the data from https://www.nasdaq.com/market-activity/funds-and-etfs/iwo/historical")
+        exit(1)
 
     print("=================== DATA PREP =============================")
     print(historicalData.head())
 
-    ema_span_low = 5
-    ema_span_high = 10
-    ema_span_long_term = 120
-    reInvesting = True
+    if subsetSize is not None:
+        subset = historicalData.head(subsetSize)
+    else:
+        subset = historicalData
 
-    subset = historicalData.head(subsetSize)
     subset = subset.sort_index()
-    price_at_start = subset.iloc[0].values[0]
-    price_at_end = subset.iloc[-1].values[0]
 
-    start_date = subset.iloc[0].name.strftime(
-        '%B %Y')
-    end_date = subset.iloc[-1].name.strftime(
-        '%B %Y')
+    ema_span_low_space = [3, 4, 5, 6, 7, 8, 9, 10]
+    ema_span_high_space = [x + 5 for x in ema_span_low_space]
+    ema_span_long_term_space = [105, 110, 115, 120, 125, 130, 135]
 
     fund_days = (subset.iloc[-1].name - subset.iloc[0].name).days
 
-    shares_owned = math.ceil(initial_investment/price_at_start)
+    maxL = 0
+    hyperParameters = None
+    performance = None
+    if args.optimize:
+        for ema_span_long_term in ema_span_long_term_space:
+            for ema_span_high in ema_span_high_space:
+                for ema_span_low in ema_span_low_space:
+                    results = simulate(ema_span_low, ema_span_high,
+                                       ema_span_long_term, subset, fund_days)
+                    if results[0] > maxL:
+                        maxL = results[0]
+                        performance = results
+                        hyperParameters = (
+                            ema_span_low, ema_span_high, ema_span_long_term)
 
-    print("=================== INDICES CALCULATION =============================")
+        hedged_cagr, hedged_portofolio_value, cash, hold_value, hold_cagr, annual_profits,  narrow_ema, wide_ema, long_term_ema, hedged = performance
+        hedged_terminal_value = hedged_portofolio_value + cash
+        print("Simulation Settings")
+        print("Fund duration: {} days (aprx {:.2f} years)".format(
+            fund_days, fund_days / 365))
+        print("Initial capital: {}".format(initial_investment))
 
-    # Calculation of indices
-    narrow_ema = subset.ewm(span=ema_span_low, adjust=False).mean()
-
-    wide_ema = subset.ewm(span=ema_span_high, adjust=False).mean()
-
-    long_term_ema = subset.ewm(
-        span=ema_span_long_term, adjust=False).mean()
-
-    narrow_shifted = narrow_ema.shift(1)
-    wide_shifted = wide_ema.shift(1)
-
-    down_momentum = ((narrow_ema <= wide_ema) &
-                     (narrow_shifted >= wide_shifted))
-
-    up_momentum = ((narrow_ema >= wide_ema) & (narrow_shifted <= wide_shifted))
-
-    # Possible index -1 error.
-    print(up_momentum.loc[up_momentum['Close/Last'] == True])
-    print(down_momentum.loc[down_momentum['Close/Last'] == True])
-
-    # Hedging Strategy Analysis
-    print("=================== HEDGING ANALYSIS ============================")
-    """
-    You are hedged when:
-        1. narrow EMA < wide EMA
-        2. price < long_term_ema
-    """
-    hedged = (subset < long_term_ema) & (narrow_ema <= wide_ema)
-    hedged_shifted_r = hedged.shift(1)
-    hedged_shifted_l = hedged.shift(-1)
-
-    hedge_start = hedged_shifted_l & (~hedged)
-    hedge_start = hedge_start.loc[hedge_start['Close/Last'] == True]
-    hedge_end = hedged_shifted_r & (~hedged)
-    hedge_end = hedge_end.loc[hedge_end['Close/Last'] == True]
-
-    # TODO: There might be an awkard case where the do the backtesting while
-    # we already hedged. In that case this might break since the dataframes
-    # do not have the same shape.
-
-    print("Fund duration: {} days (aprx {:.2f} years)".format(
-        fund_days, fund_days / 365))
-    print("Initial capital: {}".format(initial_investment))
-
-    if reInvesting:
-        print("Profits/losses made from hedging will be re-invested in the fund")
-    else:
-        print("Profits/losses made will be kept as cash")
-
-    cash = 0
-    current_shares = shares_owned
-    annual_profits = defaultdict(int)
-    for i in range(0, min(hedge_start.shape[0], hedge_end.shape[0])):
-        start = hedge_start.iloc[i].name
-        end = hedge_end.iloc[i].name
-        end_year = end.strftime('%Y')
-        hedgeProfits = short_shell(start, end, subset,
-                                   current_shares, hedging_percentage)
-        annual_profits[end_year] += hedgeProfits
+        print("Optimal Hyper-Parameters")
+        print("-> Hyper-parameters: (Narrow window, Wide window, Long Term window) ({},{},{}))".format(
+            ema_span_low, ema_span_high, ema_span_long_term))
         if reInvesting:
-            price_at_hedge_end = subset.loc[end].values[0]
-            # If profits from hedging are positive move again to the position
-            if hedgeProfits > 0:
-                shares = math.floor(hedgeProfits / price_at_hedge_end)
-                current_shares += shares
-                cash += hedgeProfits - shares * price_at_hedge_end
-            else:
-                shares = math.ceil(abs(hedgeProfits) / price_at_hedge_end)
-                current_shares -= shares
-                residual = shares * price_at_hedge_end - hedgeProfits
-                if current_shares < 0:
-                    raise ValueError(
-                        "Simulation lead to bankcrupcy at hedge {} - {}".format(start, end))
+            print("Profits/losses made from hedging will be re-invested in the fund")
         else:
-            cash += hedgeProfits
+            print("Profits/losses made will be kept as cash")
+        print("Results Overview:")
+        print("-> Terminal value with Long & Hold {:.2f} and a CAGR of {:.2f}%".format(
+            hold_value, hold_cagr))
+        print("-> Terminal value with Hedges {:.2f} (Portofolio: {:.2f} Cash: {:.2f}) and a CAGR of {:.2f}%".format(
+            hedged_terminal_value, hedged_portofolio_value, cash, hedged_cagr))
 
-    hold_value = price_at_end * shares_owned
-    hold_cagr = cagr(hold_value, initial_investment, fund_days)
-
-    hedged_portofolio_value = price_at_end * current_shares
-    hedged_terminal_value = hedged_portofolio_value + cash
-    hedged_cagr = cagr(hedged_terminal_value, initial_investment, fund_days)
-
-    print("Results Overview:")
-    print("-> Terminal value with Long & Hold {:.2f} and a CAGR of {:.2f}%".format(
-        hold_value, hold_cagr))
-    print("-> Terminal value with Hedges {:.2f} (Portofolio: {:.2f} Cash: {:.2f}) and a CAGR of {:.2f}%".format(
-        hedged_terminal_value, hedged_portofolio_value, cash, hedged_cagr))
-
-    if hedged_cagr > hold_cagr:
-        print(
-            "-> Hedging outperforms Long & Hold by {:.2f}x".format(hedged_terminal_value/hold_value))
+        if hedged_cagr > hold_cagr:
+            print(
+                "-> Hedging outperforms Long & Hold by {:.2f}x".format(hedged_terminal_value/hold_value))
+        else:
+            print(
+                "-> Long & Hold outperforms hedging by {:.2f}x".format(hold_value/hedged_terminal_value))
+        print("Hedging Cost Breakdown per Year")
+        for key in annual_profits:
+            print("-> Year: {} - Profit/Loss: {:.2f} ".format(key,
+                                                              annual_profits[key]))
     else:
-        print(
-            "-> Long & Hold outperforms hedging by {:.2f}x".format(hold_value/hedged_terminal_value))
-    print("Hedging Cost Breakdown per Year")
-    for key in annual_profits:
-        print("-> Year: {} - Profit/Loss: {:.2f} ".format(key,
-                                                          annual_profits[key]))
-
-    print("=================================================================")
+        ema_span_low = 5
+        ema_span_high = 10
+        ema_span_long_term = 120
+        performance = simulate(ema_span_low, ema_span_high,
+                               ema_span_long_term, subset, fund_days, verbose=True)
 
     if args.visualize:
+        hedged_cagr, hedged_portofolio_value, cash, hold_value, hold_cagr, annual_profits,  narrow_ema, wide_ema, long_term_ema, hedged = performance
         # Visualization Layer
+        start_date = subset.iloc[0].name.strftime(
+            '%B %Y')
+        end_date = subset.iloc[-1].name.strftime(
+            '%B %Y')
+
         ax = subset.rename(columns={"Close/Last": "IWO Price"}).plot(lw=1,
                                                                      figsize=(14, 7), label='IWO Technical Analysis')
 
